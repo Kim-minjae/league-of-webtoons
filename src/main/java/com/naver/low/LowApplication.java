a1a5ea1ce6092b850aa45041ca1b53c3e7cc0cad
minjae-Kim <pose2done@naver.com>
2018-04-05 00:27:41 +0900

    git init

---
+++ src/main/java/com/naver/low/LowApplication.java
@@ -0,0 +1,12 @@
+package com.naver.low;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowApplication.class, args);
+	}
+}
