640ed85c31fac89167373a300da13646f79b40e3
BAN-m <dazedbj@gmail.com>
2018-04-06 01:14:04 -0400

    .http files and usercontroller added

--- src/main/java/com/naver/low/controllers/UserController.java
+++ src/main/java/com/naver/low/controllers/UserController.java
@@ -1,5 +1,13 @@
 package com.naver.low.controllers;
 
+import com.naver.low.payloads.UserSummary;
+import com.naver.low.repositories.UserRepository;
+import com.naver.low.security.CurrentUser;
+import com.naver.low.security.UserPrincipal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_WEBTOONIST')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getEmail());
+        return userSummary;
+    }
 }
--- src/main/java/com/naver/low/repositories/UserRepository.java
+++ src/main/java/com/naver/low/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserEmail(String email);
+
     Boolean existsByUserEmail(String email);
 
 }
