7ad841b5111544349d679864d379fd4465b2162f
BAN-m <dazedbj@gmail.com>
2018-04-09 22:20:00 -0400

    .

--- src/main/java/com/naver/low/controllers/WebtoonController.java
+++ src/main/java/com/naver/low/controllers/WebtoonController.java
@@ -1,12 +1,17 @@
 package com.naver.low.controllers;
 
+import com.naver.low.payloads.ApiResponse;
 import com.naver.low.payloads.CreateWebtoonRequest;
 import com.naver.low.repositories.WebtoonRepository;
+import com.naver.low.security.CurrentUser;
+import com.naver.low.security.UserPrincipal;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 @RestController
@@ -17,8 +22,18 @@ public class WebtoonController {
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_WEBTOONIST')")
-    public ResponseEntity<?> uploadWebtoon (@RequestParam("file") MultipartFile file, @RequestBody CreateWebtoonRequest createWebtoonRequest) {
+    public ResponseEntity<ApiResponse> uploadWebtoon (@RequestParam("file") MultipartFile[] files,
+                                                      @RequestBody CreateWebtoonRequest createWebtoonRequest,
+                                                      @CurrentUser UserPrincipal curentUser) {
+        if (files == null) {
+            // when a user doesn't attach a file, which http status code should be returned?
+            return ResponseEntity.ok(new ApiResponse(false, "please select a file"));
+        }
+
+        // saveFiles(Arrays.asList(files), )
+
         return null;
     }
+
 
 }
